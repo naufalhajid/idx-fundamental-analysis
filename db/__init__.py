70332db8b7f2c031a5324d965048ca06cb149de6
Satrya Budi Pratama <satrya@zeroinside.id>
2025-11-30 14:56:49 +0700

    refactor: configure logging levels and add dynamic log level support from environment variable

--- db/__init__.py
+++ db/__init__.py
@@ -19,11 +19,14 @@ base_dir = get_project_root()
 db_path = os.path.join(base_dir, "db/idx-fundamental.db")
 
 logging.basicConfig(handlers=[InterceptHandler()], level=0)
+logging.getLogger("sqlalchemy").setLevel(logging.WARNING)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+logging.getLogger("sqlalchemy.pool").setLevel(logging.WARNING)
 
 
 class DB:
     def __init__(self):
-        self._engine = create_engine(f"sqlite:///{db_path}", echo=True)
+        self._engine = create_engine(f"sqlite:///{db_path}", echo=False)
 
     def setup_db(self, is_drop_table: bool = False):
         with self._engine.begin() as conn:
--- utils/logger_config.py
+++ utils/logger_config.py
@@ -1,12 +1,37 @@
 import logging
+import os
+import sys
 
+from dotenv import load_dotenv
 from loguru import logger
+
+load_dotenv()
+
+level = os.getenv("LOG_LEVEL", "INFO")
+
+# Remove default Loguru sink (stderr, level=DEBUG)
+logger.remove()
+
+# Console sink with dynamic level
+logger.add(
+    sys.stderr,
+    format=(
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{file}</cyan>:<cyan>{line}</cyan> "
+        "<cyan>{function}</cyan> "
+        "<level>{message}</level>"
+    ),
+    level=level,
+    colorize=True,  # ensure ANSI colors are used
+)
+
 
 # Configure the logger to write to a log file
 logger.add(
     "logs/app.log",
-    format="{time} {level} {message}",
-    level="DEBUG",
+    format=("{time:YYYY-MM-DD HH:mm:ss.SSS} {level: <8} {file}:{line} | {message}"),
+    level=level,
     rotation="1 MB",  # Rotate after the log file reaches 1 MB
     retention="10 days",  # Keep rotated files for 10 days
     compression="zip",  # Compress rotated files
